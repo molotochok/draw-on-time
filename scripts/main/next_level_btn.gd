b7c76c2651fcecca8d6b9bf9ffc99f2c9c249a91
Markiian Karpa <markian98@gmail.com>
2022-01-29 13:08:41 +0200

    Add next level button

--- scripts/main/next_level_btn.gd
+++ scripts/main/next_level_btn.gd
@@ -1,4 +1,17 @@
-extends Button
+extends TextureButton
+
+func _init():
+	assert(GameEvents.connect("settings_initialized", self, "_on_settings_initialized") == OK)
+	assert(GameEvents.connect("settings_updated", self, "_on_settings_updated") == OK)
 
 func _pressed():
   LevelManager.load_next_level()
+
+func _on_settings_initialized(settings: Settings):
+	change_visibility(settings)
+
+func _on_settings_updated(settings: Settings):
+	change_visibility(settings)
+
+func change_visibility(settings: Settings):
+	set_visible(settings.passed())
