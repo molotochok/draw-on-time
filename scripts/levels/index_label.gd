c89287105a2d1a93c9492eb9961c0f6da360d527
Markiian Karpa <markian98@gmail.com>
2022-02-02 22:37:01 +0200

    Redraw album

--- scripts/levels/index_label.gd
+++ scripts/levels/index_label.gd
@@ -7,6 +7,6 @@ func _on_resized():
 	var font: DynamicFont = get_font("level_button_index_font")
 	var size = OS.get_window_size()
 	
-	font.size = int(Math.linear_extrapolation(size.x, 815, 10, 1500, 20))
+	font.size = int(Math.linear_extrapolation(size.x, 815, 10, 1500, 18))
 	
 	add_font_override("level_button_index_font", font)
