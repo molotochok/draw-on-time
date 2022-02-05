09c723be008db7250154fecf93a33ab4168219be
Markiian Karpa <markian98@gmail.com>
2022-02-05 23:14:57 +0200

    Improve UpDownMover

--- scripts/levels/level_button_mover.gd
+++ scripts/levels/level_button_mover.gd
@@ -1,9 +1,6 @@
 extends UpDownMover
 
 func _ready():
-	._ready()
-
-	assert(connect("resized", self, "_on_resized") == OK)
 	assert(connect("mouse_entered", self, "_on_mouse_entered") == OK)
 	assert(connect("mouse_exited", self, "_on_mouse_exited") == OK)
 
@@ -17,11 +14,6 @@ func _on_mouse_exited():
 	if interactable():
 		_move_dir = MoveDirection.DOWN
 
-func _on_resized():
-	update_initial_parent_pos()
-
 func interactable() -> bool:
 	return modulate.a > 0 && _parent.opened()
 
-func update_initial_parent_pos():
-	_initial_parent_pos = _parent.get_rect().position	
--- scripts/up_down_mover.gd
+++ scripts/up_down_mover.gd
@@ -9,7 +9,8 @@ enum MoveDirection {
 }
 
 export(float) var _step := 1.0
-export(int) var _max_offset := 8
+export(float) var _max_offset_up := 8
+export(float) var _max_offset_down := 8
 export(bool) var should_stop := true
 export(MoveDirection) var _move_dir := MoveDirection.IDLE
 
@@ -17,7 +18,9 @@ onready var _parent: Control = get_parent() as Control
 onready var _initial_parent_pos: Vector2
 
 func _ready():
-  update_initial_parent_pos()
+	update_initial_parent_pos()
+
+	assert(connect("resized", self, "_on_resized") == OK)
 
 func _physics_process(delta):
 	if (_move_dir == MoveDirection.UP):
@@ -25,11 +28,14 @@ func _physics_process(delta):
 	
 	if (_move_dir == MoveDirection.DOWN):
 		move_down(delta)
-		
+
+func _on_resized():
+	update_initial_parent_pos()
+
 func move_up(delta):
 	var pos = _parent.get_rect().position
 	
-	if(_initial_parent_pos.y - pos.y >= _max_offset):
+	if(_initial_parent_pos.y - pos.y >= _max_offset_up):
 		_move_dir = MoveDirection.IDLE if should_stop else MoveDirection.DOWN
 	else:
 		_parent.set_position(Vector2(pos.x, pos.y - _step * delta))
@@ -37,10 +43,10 @@ func move_up(delta):
 func move_down(delta):
 	var pos = _parent.get_rect().position
 	
-	if pos.y - _initial_parent_pos.y >= _max_offset:
+	if pos.y - _initial_parent_pos.y >= _max_offset_down:
 		_move_dir = MoveDirection.IDLE if should_stop else MoveDirection.UP
 	else:
 		_parent.set_position(Vector2(pos.x, pos.y + _step * delta))
 
 func update_initial_parent_pos():
-  _initial_parent_pos = _parent.get_rect().position	
+	_initial_parent_pos = _parent.get_rect().position	
