36712bef93fd9a912b913db9b940360c71661150
Markiian Karpa <markian98@gmail.com>
2022-02-11 06:26:57 +0200

    Add FinishMessageDisplayer

---
+++ scripts/main/finish_messages_displayer.gd
@@ -0,0 +1,39 @@
+extends Control
+
+export(NodePath) onready var label = get_node(label) as Label
+export(NodePath) onready var anim_player = get_node(anim_player) as AnimationPlayer
+export(Resource) onready var finish_messages = finish_messages as FinishMessages
+
+export(Array, Color) onready var label_colors = label_colors as Array
+
+func _ready():
+  assert(GameEvents.connect("score_calculated", self, "_on_score_calculated") == OK)
+  assert(GameEvents.connect("refreshed", self, "_on_refreshed") == OK)
+
+  hide()
+
+func _on_refreshed():
+  hide()
+
+func _on_score_calculated(score: float):
+  show_message(Settings.get_stars_by(score))
+  show_animated()
+
+func show_animated():
+  anim_player.play("finish_message_displayer")
+
+func show_message(stars: int):
+  label.text = "%s\n%s!" % [build_default_message(stars), get_message(stars)]
+  label.modulate = label_colors[stars]
+
+func build_default_message(stars: int) -> String:
+  var message = "You've earned %s star" % str(stars)
+
+  if stars != 1:
+    message += "s"
+  
+  return message
+
+func get_message(stars: int) -> String:
+  var messages = finish_messages.messages[stars]
+  return messages[randi() % messages.size()]
---
+++ scripts/resources/finish_messages.gd
@@ -0,0 +1,6 @@
+# The messages that are shown to the gamer after he finishes the level
+extends Resource
+
+class_name FinishMessages
+
+export(Array, Array, String) var messages = [[]]
